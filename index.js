aea59cf0b0213499f7748ada51d4bed14971e97f
Maia Donoga <medeea@rocketmail.com>
2018-01-05 13:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector("#nested .target");
 }
+
+function increaseRankBy(n) {
+  var el = document.querySelectorAll(".ranked-list li");
+  for (var i in el) {
+    el[i].innerHTML = +el[i].innerHTML + n;
+  }
+}
+
+function deepestChild() {
+  return (Array.from(document.querySelectorAll("#grand-node div"))).pop();
+}
